5f27665ae3c9699650bff3d40dd451cd1677da6c
Admin <natali.sadilova@inbox.ru>
2026-04-27 16:12:36 +0300

    URL маршруты для продуктов

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    # API для поставщиков (магазинов)
+    path('partner/update/', views.PartnerUpdate.as_view(), name='partner-update'),
+    path('partner/state/', views.PartnerState.as_view(), name='partner-state'),
+    path('partner/orders/', views.PartnerOrders.as_view(), name='partner-orders'),
+    
+]
