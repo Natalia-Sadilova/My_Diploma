3292c13d13d1a55f8793b59e6a579efa05fadad3
Admin <natali.sadilova@inbox.ru>
2026-04-27 14:51:28 +0300

    модель магазина

--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    url = models.URLField(verbose_name='Ссылка', null=True, blank=True)
+    user = models.OneToOneField(
+        User, 
+        verbose_name='Пользователь',
+        blank=True, 
+        null=True,
+        on_delete=models.CASCADE
+    )
+    state = models.BooleanField(
+        verbose_name='Статус получения заказов', 
+        default=True
+    )
+
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = "Список магазинов"
+        ordering = ('-name',)
+
+    def __str__(self):
+        return self.name
+
