4cf0289da9a2976e85c5e0847645953909826b36
Admin <natali.sadilova@inbox.ru>
2026-04-27 14:50:14 +0300

    модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,15 +1,23 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django_rest_passwordreset.tokens import get_token_generator
+
+USER_TYPE_CHOICES = (
+    ('shop', 'Магазин'),
+    ('buyer', 'Покупатель'),
+)
 
 
 class UserManager(BaseUserManager):
+    """Миксин для управления пользователями"""
     use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('The given email must be set')
+            raise ValueError('Данный адрес электронной почты должен быть установлен')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -24,33 +32,91 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+
         return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    USER_TYPE_CHOICES = (
-        ('shop', 'Магазин'),
-        ('buyer', 'Покупатель'),
-    )
-    
+    """Стандартная модель пользователей"""
     REQUIRED_FIELDS = []
     objects = UserManager()
     USERNAME_FIELD = 'email'
     
     email = models.EmailField(_('email address'), unique=True)
-    company = models.CharField(max_length=40, blank=True, verbose_name='Компания')
-    position = models.CharField(max_length=40, blank=True, verbose_name='Должность')
-    type = models.CharField(
-        max_length=5,
-        choices=USER_TYPE_CHOICES,
-        default='buyer',
-        verbose_name='Тип пользователя'
+    company = models.CharField(verbose_name='Компания', max_length=40, blank=True)
+    position = models.CharField(verbose_name='Должность', max_length=40, blank=True)
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        help_text=_('Требуется 150 символов или меньше. Буквы, цифры и @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("Пользователь с таким именем уже существует."),
+        },
     )
-    is_active = models.BooleanField(default=False, verbose_name='Активен')
+    is_active = models.BooleanField(
+        _('active'),
+        default=False,
+        help_text=_(
+            'Определяет, следует ли считать этого пользователя активным. '
+            'Отмените выбор вместо удаления аккаунтов.'
+        ),
+    )
+    type = models.CharField(
+        verbose_name='Тип пользователя', 
+        choices=USER_TYPE_CHOICES, 
+        max_length=5, 
+        default='buyer'
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
     class Meta:
         verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name_plural = "Список пользователей"
+        ordering = ('email',)
+
+
+class ConfirmEmailToken(models.Model):
+    """Токен подтверждения Email"""
+    class Meta:
+        verbose_name = 'Токен подтверждения Email'
+        verbose_name_plural = 'Токены подтверждения Email'
+
+    @staticmethod
+    def generate_key():
+        return get_token_generator().generate_token()
+
+    user = models.ForeignKey(
+        User,
+        related_name='confirm_email_tokens',
+        on_delete=models.CASCADE,
+        verbose_name=_("Пользователь, связанный с этим токеном сброса пароля")
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Дата генерации токена")
+    )
+
+    key = models.CharField(
+        _("Key"),
+        max_length=64,
+        db_index=True,
+        unique=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.email
+        return f"Токен подтверждения электронной почты для пользователя {self.user}"
